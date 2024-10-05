f31bf082375e4bd6ac9e978a0b13a59cdcb45e98
jefftrojan <j.dauda@alustudent.com>
2024-10-05 15:35:07 +0200

    Add business dashboard

---
+++ app/(site)/businesses/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from 'react';
+import Dashboard from './dashboard';
+import { useState } from 'react';
+
+const BusinessesPage: React.FC = () => {
+    return (
+        <div>
+    
+            <Dashboard />
+        </div>
+    );
+};
+
+export default BusinessesPage;
